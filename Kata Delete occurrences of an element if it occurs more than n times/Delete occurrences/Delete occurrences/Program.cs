2472add5aa448cc21cf9fd6e9e66a65899fecff1
Ritik <ritik.sharma@taazaa.com>
2021-05-10 17:22:39 +0530

    Delete occurrences of an element if it occurs more than n times

---
+++ Kata Delete occurrences of an element if it occurs more than n times/Delete occurrences/Delete occurrences/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Delete_occurrences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] r = DeleteNth(new int[] { 1, 2, 3, 1, 2, 3, 5, 1 }, 1);
+            r.ToList().ForEach(x => Console.WriteLine(x));
+        }
+        public static int[] DeleteNth(int[] arr, int x)
+        {
+            List<int> result = new List<int>();
+            foreach (int i in arr) {
+                if (result.Count(item => item == i) < x)
+                {
+                    result.Add(i);
+                }
+            }
+            return result.ToArray();
+        }
+    }
+}
