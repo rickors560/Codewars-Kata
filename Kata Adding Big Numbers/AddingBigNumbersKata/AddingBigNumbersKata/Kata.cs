6cfb27cbc8c4eb20c81bb81b4c5aa18bf05708e2
Ritik <ritik.sharma@taazaa.com>
2021-06-19 22:50:03 +0530

    Add two big numbers as string and return result in string

---
+++ Kata Adding Big Numbers/AddingBigNumbersKata/AddingBigNumbersKata/Kata.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddingBigNumbersKata
+{
+    public class Kata
+    {
+        public static string Add(string a, string b)
+        {
+            var num1 = BigInteger.Parse(a);
+            var num2 = BigInteger.Parse(b);
+
+            var result = BigInteger.Add(num1, num2);
+            return result.ToString();
+        }
+    }
+}
---
+++ Kata Adding Big Numbers/AddingBigNumbersKata/AddingBigNumbersKata/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AddingBigNumbersKata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = Kata.Add("123", "321");
+            Console.WriteLine(result);
+        }
+    }
+}
