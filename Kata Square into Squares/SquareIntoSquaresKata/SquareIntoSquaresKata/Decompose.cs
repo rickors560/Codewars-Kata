e80ec45b21573db71859ee535a7cc355d5a71354
Ritik <ritik.sharma@taazaa.com>
2021-06-20 23:40:03 +0530

    Recursive approach to find solution

---
+++ Kata Square into Squares/SquareIntoSquaresKata/SquareIntoSquaresKata/Decompose.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareIntoSquaresKata
+{
+	public class Decompose
+	{
+        public static string decompose(long n)
+        {
+            var list = new List<long>();
+            if (Divide(list, n * n, n))
+                return string.Join(" ", list);
+
+            return null;
+        }
+
+        private static bool Divide(List<long> numbers, double remain, long last)
+        {
+            if (remain <= 0)
+                return remain == 0;
+
+            for (var i = last - 1; i > 0; i--)
+            {
+                if (Divide(numbers, remain - (i * i), i))
+                {
+                    numbers.Add(i);
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Kata Square into Squares/SquareIntoSquaresKata/SquareIntoSquaresKata/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SquareIntoSquaresKata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = Decompose.decompose(50);
+            Console.WriteLine(result);
+        }
+    }
+}
